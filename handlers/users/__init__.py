0d378c1368edb9495c0f133753cd93a681bdef28
Usmonaliyev Salohiddin <108422142+salohiddinusmonaliyev@users.noreply.github.com>
2024-03-03 15:33:18 +0500

    v1

--- handlers/users/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import help
-from . import start
-from . import mainHandlers
-# from . import inlinemode
-from . import echo
--- handlers/users/ad_handle.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import Message
-from loader import dp, bot
-
-@dp.message_handler()
-async def ad():
-    await bot.send_message(chat_id=None, text="Reklama")
